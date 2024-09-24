e05ef2385a8e40552be8ca57ebc725bf88884230
Samuel Ubuntu <2211600020@fema.edu.br>
2024-09-24 08:21:45 -0300

    feat(minor): criar pagina de login

---
+++ src/Screens/Login/index.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as Native from "react-native"
+
+export function Login() {
+    return (
+        <Native.View>
+            <Native.Text>Tela Login</Native.Text>
+        </Native.View>
+    );
+}
