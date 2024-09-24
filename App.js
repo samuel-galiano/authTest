ec09d1dc67a5dc5f70765d048fa4f4f97fae251e
Samuel Ubuntu <2211600020@fema.edu.br>
2024-09-24 11:51:20 -0300

    style(minor): alterar as cores de azul para vermelho

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Routes from './src/Routes';
 export default function App() {
   return (
       <Navigation.NavigationContainer>
-          <Native.StatusBar backgroundColor={"#38A69D"} barStyle={'light-content'} />
+          <Native.StatusBar backgroundColor={"#86101D"} barStyle={'light-content'} />
           <Routes/>
       </Navigation.NavigationContainer>
   );
