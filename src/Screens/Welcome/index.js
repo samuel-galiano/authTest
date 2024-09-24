52443e5caf93357f559a4f694a966719b20cda4d
Samuel Ubuntu <2211600020@fema.edu.br>
2024-09-24 12:18:47 -0300

    move(minor): mover estilização do componente e move-lo para outro arquivo

--- src/Screens/Welcome/index.js
+++ src/Screens/Welcome/index.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as Native from "react-native"
 import * as Animatable from "react-native-animatable"
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native"
+import { welcomeStyle } from "./style";
 
 export function Welcome() {
     const navigation = useNavigation()
@@ -20,45 +21,4 @@ export function Welcome() {
     );
 }
 
-const styles = Native.StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#86101D",
-    },
-    containerForm: {
-        flex: 1,
-        backgroundColor: "#ebebeb",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20
-    },
-    containerText: {
-        flex: 6,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    title: {
-        color: "#ebebeb",
-        fontSize: 40,
-        fontWeight: "bold",
-    },
-    text: {
-        color: "#ebebeb",
-        fontSize: 20
-    },
-    button: {
-        backgroundColor: "#86101D",
-        padding: 20,
-        borderRadius: 10,
-        width: '60%'
-    },
-    buttonText: {
-        color: "#ebebeb",
-        justifyContent: "center",
-        alignContent: "center",
-        alignSelf: "center",
-        fontWeight: "bold"
-    }
-})
+const styles = Native.StyleSheet.create( welcomeStyle )
---
+++ src/Screens/Welcome/style.js
@@ -0,0 +1,42 @@
+export const welcomeStyle = {
+    container: {
+        flex: 1,
+        backgroundColor: "#86101D",
+    },
+    containerForm: {
+        flex: 1,
+        backgroundColor: "#ebebeb",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20
+    },
+    containerText: {
+        flex: 6,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    title: {
+        color: "#ebebeb",
+        fontSize: 40,
+        fontWeight: "bold",
+    },
+    text: {
+        color: "#ebebeb",
+        fontSize: 20
+    },
+    button: {
+        backgroundColor: "#86101D",
+        padding: 20,
+        borderRadius: 10,
+        width: '60%'
+    },
+    buttonText: {
+        color: "#ebebeb",
+        justifyContent: "center",
+        alignContent: "center",
+        alignSelf: "center",
+        fontWeight: "bold"
+    }
+}
